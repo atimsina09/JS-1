95e5bed8fb0c61f660e6c96e1394c9a39f2ace0a
atimsina09 <ahaantimsina@gmail.com>
2022-09-17 20:46:15 -0500

    idk

---
+++ js.js
@@ -0,0 +1,22 @@
+//console.log("Hello World")
+
+/*var age = 45;
+var name = "Benjaman";
+var profession = "McDonalds Worker";
+var salary = 10000;
+console.log(name+" is "+age+" years old, and he is a "+profession+" and makes "+salary+" dollars every year.")
+
+var kg = 50
+var h = 1.74
+var bmi = kg/h
+
+console.log(name+" has a bmi of "+bmi)
+
+var bmi2 = kg/(h*h)
+
+console.log(name+" has a bmi of "+bmi2)*/
+
+var kg = prompt("Enter your weight in kg's")
+var h = prompt("Enter your height in meters")
+var bmi = kg/(h*h)
+alert(bmi)
